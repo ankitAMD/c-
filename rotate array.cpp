e2e5032ac9cbc6a619a3adc776b89e7a48257fce
Ankit Gupta <gankit974@gmail.com>
2019-06-02 17:38:25 +0530

    Update rotate array.cpp

--- rotate array.cpp
+++ rotate array.cpp
@@ -1,3 +1,26 @@
+
+#Program for array rotation
+
+/*Write a function rotate(ar[], d, n) that rotates arr[] of size n by d elements.
+Array
+
+Rotation of the above array by 2 will make array
+
+ArrayRotation1
+
+
+Input arr[] = [1, 2, 3, 4, 5, 6, 7], d = 2, n =7
+1) Store d elements in a temp array
+   temp[] = [1, 2]
+2) Shift rest of the arr[]
+   arr[] = [3, 4, 5, 6, 7, 6, 7]
+3) Store back the d elements
+   arr[] = [3, 4, 5, 6, 7, 1, 2]
+
+
+*/
+
+
 #include <iostream>
 using namespace std;
 
